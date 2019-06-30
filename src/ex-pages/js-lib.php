b95c6e4bf1f5699a14ff4d1a2f81b68d431ef8f9
F4MED <admin@f4med.com>
2019-06-30 15:23:09 +0300

    remove ex-pages files

--- src/ex-pages/js-lib.php
+++
@@ -1,7 +0,0 @@
-<?php
-$js_list = $this->db->query('SELECT * FROM pages where type="css"');
-
-while($row = $js_list->fetch()){
-  echo "\n".' <script src="'.DOMAIN.$row["route"].'"></script>';
-}
- ?>
