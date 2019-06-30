7a3e1f6b5f681894081c1d7d76311856a67fce01
F4MED <admin@f4med.com>
2019-07-01 01:37:42 +0300

    re-edited js methods

--- src/methods/js.php
+++ src/methods/js.php
@@ -6,20 +6,19 @@ $this->addMethod("js_lib",function(){
   $js_list = explode("|",$js_list["value"]);
   foreach ($js_list as $row) {
     if( isset($lib[ $row ]) && isset( $lib[ $row ]["js"] ) ):
-      echo "\n".' <script src="'.DOMAIN.$lib[$row]["js"]["path"].'"></script>';
+      echo '<script src="'.DOMAIN.$lib[$row]["js"]["path"].'"></script>';
     endif;
   }
 });
 $this->addMethod("js_local",function($params = null){
-  $where = setParams($params,"where") != null ? setParams($params,"where") : setParams($params,0);
+  $where = setParams($params,0);
   $js_file_list_dir = DIR."/asset/js";
   $js_file_list = scandir($js_file_list_dir);
   $js_file_list = array_diff($js_file_list,[".",".."]);
   $js_file_list = array_diff($js_file_list,$where);
   foreach ($js_file_list as $row) {
 
-    echo "\n\t".'<script src="'.DOMAIN.'/asset/js/'.$row.'"></script>';
+    echo '<script src="'.DOMAIN.'/asset/js/'.$row.'"></script>';
   }
 });
-$this->js_local();
 ?>
