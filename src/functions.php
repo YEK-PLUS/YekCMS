aa4de8868f693720f336f668ea45d17eb1e50bfe
F4MED <admin@f4med.com>
2019-06-30 23:08:31 +0300

    fix function param null problem

--- src/functions.php
+++ src/functions.php
@@ -5,7 +5,7 @@ function printSVG($name){
   }
 }
 function setParams($params,$key){
-  return ( ($params != null) && (gettype($params) == "array") ) && ($params[$key] != null) ? $params : null;
+  return ( ($params != null) && (gettype($params) == "array") ) && ($params[$key] != null) ? $params : [];
 }
 
  ?>
