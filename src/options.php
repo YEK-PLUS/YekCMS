4813fc16adebc589d8d416030b35d03b437cd8a2
F4MED <admin@f4med.com>
2019-06-30 23:09:04 +0300

    added options file

---
+++ src/options.php
@@ -0,0 +1,16 @@
+<?php
+$lib = [];
+
+$lib["bootstrap"] = [
+  "css" => [
+      "path" => "/lib/Bootstrap/css/bootstrap.css",
+      "min_path" => "/lib/Bootstrap/css/bootstrap.min.css"
+  ],
+  "js" => [
+    "path" => "/lib/Bootstrap/js/bootstrap.js",
+    "min_path" => "/lib/Bootstrap/js/bootstrap.min.js"
+  ]
+];
+
+
+?>
