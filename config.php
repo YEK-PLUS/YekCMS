beae79fe0a29f7e91f54c1a9f4c45b15a5b35019
YEK.PLUS <yek.plus.plus@gmail.com>
2019-07-22 02:13:22 +0300

    update config for travis

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 define("DB_CONF",[
   "database_type" => "mysql",
-  "database_name" => "",
+  "database_name" => "root",
   "server" => "localhost",
   "username" => "root",
   'charset' => 'utf8',
-  "password" => ""]);
+  "password" => "password"]);
  ?>
