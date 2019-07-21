580082a97a1da1d24892bce77c58bac768fe8d57
YEK.PLUS <yek.plus.plus@gmail.com>
2019-07-22 02:08:59 +0300

    adding config file

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+define("DB_CONF",[
+  "database_type" => "mysql",
+  "database_name" => "",
+  "server" => "localhost",
+  "username" => "root",
+  'charset' => 'utf8',
+  "password" => ""]);
+ ?>
